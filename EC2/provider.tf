25340ee89e12377160d508d04c27de79c043027e
Shi191099 <shital191099@gmail.com>
2022-08-29 13:47:54 +0530

    update

---
+++ EC2/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIASWSDQ4H4K2L4T7UH"
+  secret_key = "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"
+  region = "us-west-2" 
+}
---
+++ EC2/variable.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+    default = "10.0.0.0/16" 
+}
+variable "subnet_cidr" {
+    default = "10.0.1.0/24" 
+}
+variable "azs" {
+    default = "us-west-2a"
+}
+variable "aws_region" {
+    default = "us-west-2a"
+}
